6df3fcadb29346f8ade1ac34a843fec2be6ffcf0
usuario <usuario@MININT-24CL7RV>
2018-01-17 16:55:20 -0200

    Set

--- src/br/com/colicon/teste/Reflexao.java
+++ src/br/com/colicon/teste/Reflexao.java
@@ -1,21 +1,56 @@
 package br.com.colicon.teste;
 
+import java.awt.Window.Type;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Reflexao {
 
-	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
 		Contrato contrato = new Contrato();
-		contrato.setContratada("Contratada");
-		contrato.setContratoId(1); // o atributo tem que ser público
-		//System.out.println("Start");
-		//getAtributosNulos(contrato);
-		//System.out.println("Campos não nulos: " + getAtributosNulos(contrato));
+		// contrato.setContratada("Contratada");
+		// contrato.setContratoId(1); // o atributo tem que ser público
+		// System.out.println("Start");
+		// getAtributosNulos(contrato);
+		// System.out.println("Campos não nulos: " + getAtributosNulos(contrato));
+		// getMetodos(contrato);
+
+		// Class<?> c = contrato.getClass();
+
+		// Method setm = c.getMethod("setContratada", String.class);
+		// setm.invoke(contrato, "teste");
+
+		// Method m = c.getMethod("getContratada");
+		// String s = (String) m.invoke(contrato);
+		// System.out.println(s);
+
+		// getMetodos(contrato);
+
+		Class<?> c = contrato.getClass();
+		int i = 0;
+		for (Method m : c.getMethods()) {
+			// if (m.getName().contains("setContratada")) {
+
+			if (m.getName().contains("set")) 	{
+				Parameter [] tipos = m.getParameters();
+				for (i=0;i<tipos.length-1;i++) {}
+				//System.out.println(tipos[i]);
+				if (tipos[i] instanceof String) m.invoke(contrato,"teste");
+			}
+		}
+
 		getMetodos(contrato);
+		System.out.println(contrato);
+
+
+		
+		
 	}
 
 	public static List<String> getAtributosNulos(Object o) {
@@ -38,14 +73,39 @@ public class Reflexao {
 		}
 	}
 
-	public static void getMetodos(Object o) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public static void getMetodos(Object o)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Class<?> c = o.getClass();
+		for (Method m : c.getMethods()) {
+			System.out.println(m.getParameterTypes());
+
+		}
+	}
+
+	public static void getMetodos2(Object o)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Class<?> c = o.getClass();
 		for (Method m : c.getMethods()) {
 			// System.out.println(m);
 			if (m.getName().contains("get") && m.getReturnType() == String.class) {
 				String s = (String) m.invoke(o);
 				System.out.println(s);
-				//System.out.println(m.getName());
+				// System.out.println(m.getName());
+			}
+
+		}
+	}
+
+	public static void setStrings(Object o)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Class<?> c = o.getClass();
+		int i = 0;
+		for (Method m : c.getMethods()) {
+			if (m.getName().contains("set") && m.getReturnType() == String.class) {
+				i++;
+				m.invoke(o, "teste" + String.valueOf(i));
+				// System.out.println(s);
+				// System.out.println(m.getName());
 			}
 
 		}
